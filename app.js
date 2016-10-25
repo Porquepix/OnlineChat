3e74a2938c4dc399c4d8edc998a4fce7cfe17e37
Porquepix <porquepix@hotmail.fr>
2016-10-25 07:40:01 +0200

    Change server port

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 // Require HTTP module (to start server) and Socket.IO
 var http = require('http');
 var io = require('socket.io');
-var port = 80;
-
+const PORT = process.env.PORT || 3000;
 
 // Start the server at 80
 var server = http.createServer(function (req, res) { 
-    res.end();
+    res.end('Hello World !');
 });
-server.listen(port);
+server.listen(PORT);
 
 
 // Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
+var socket = io(server);
 
 // Create the chat instance
 require('./chat.js')(socket)();
